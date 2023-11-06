e03bfa9be13b656a3413e0132fd1460c351aed6f
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-11-06 17:52:56 +0000

    movies progress

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   def create
-    # params hash: {"the_title"=>"bn", "the_year"=>"2023", "the_duration"=>"345", "the_description"=>"interesting", "the_image"=>"na", "the_director_id"=>"23"}
+    # params hash looks like this:
+    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+
     m = Movie.new
     m.title = params.fetch("the_title")
     m.year = params.fetch("the_year")
@@ -11,11 +13,14 @@ class MoviesController < ApplicationController
 
     m.save
 
-    # Retrieve users inputs
-    # Create a record
-    # Populate each column with input
+    redirect_to("/movies")
 
-    #Redirect back to /movies
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+
+    # Redirect the user back to the /movies URL
   end
   
   def index
