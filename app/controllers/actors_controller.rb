f1b8fa912e52daba4d32a189f45958df3243b216
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-11-07 03:23:29 +0000

    finished, just a small issue with updating images

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,19 @@
 class ActorsController < ApplicationController
+  def update
+    m_id = params.fetch("the_name")
+
+    matching_records = Actor.where({:name => m_id})
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
 
   
   def create
@@ -24,6 +39,13 @@ class ActorsController < ApplicationController
     # Redirect the user back to the /movies URL
   end
 
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_records = Actor.where({:id => the_id})
+    the_actor = matching_records.at(0)
+    the_actor.destroy
+    redirect_to("/actors")
+  end
 
   def index
     matching_actors = Actor.all
